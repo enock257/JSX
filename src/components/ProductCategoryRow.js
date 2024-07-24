cc21006477418f9b6f419d42b0f8da96129df0f3
Enock <mugishanikuzeenock@gmail.com>
2024-07-24 13:03:52 +0200

    ract

--- src/components/ProductCategoryRow.js
+++
@@ -1,9 +0,0 @@
-export function ProductCategoryRow({ name }) {
-  return (
-    <tr>
-      <td colSpan={2}>
-        <strong>{name}</strong>
-      </td>
-    </tr>
-  );
-}
--- src/components/ProductRow.js
+++
@@ -1,9 +0,0 @@
-export function ProductRow({ product }) {
-  const style = product.stoked ? undefined : { color: "red" };
-  return (
-    <tr>
-      <td style={style}>{product.name}</td>
-      <td>{product.price}</td>
-    </tr>
-  );
-}
